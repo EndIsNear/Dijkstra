5ff79fb792e3ccdfc4b2a85f9fc9a6433c7ca0b3
endisnear <endisnear@abv.bg>
2014-02-18 19:40:40 +0200

    Init project. Added heap.hpp

---
+++ Dijkstra/graph.hpp
@@ -0,0 +1,10 @@
+#ifndef GRAPH_HPP_ICLUDED
+#define GRAPH_HPP_ICLUDED
+
+template<class T>
+class Graph
+{
+
+};
+
+#endif //GRAPH_HPP_ICLUDED
---
+++ Dijkstra/main.cpp
@@ -0,0 +1,7 @@
+#include"heap.hpp"
+
+int main()
+{
+
+	return 0;
+}
