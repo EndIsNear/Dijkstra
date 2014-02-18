2b3e4dbc87599ba9b43d1a51832e74170ac656fc
endisnear <endisnear@abv.bg>
2014-02-18 22:35:11 +0200

    Added graph implementation. Added graph input.

--- Dijkstra/graph.hpp
+++ Dijkstra/graph.hpp
@@ -1,9 +1,120 @@
 #ifndef GRAPH_HPP_ICLUDED
 #define GRAPH_HPP_ICLUDED
 
-template<class T>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 class Graph
 {
+	int** adjMatr;
+	size_t size;
+	size_t maxSize;
+
+	void deleteMatr()
+	{
+		for (size_t cnt = 0; cnt < size; ++cnt)
+		{
+			delete[] this->adjMatr[cnt];
+		}
+		delete[] this->adjMatr;
+	}
+
+	void resize(size_t newSize)
+	{
+		int ** newMatr;
+		copyMatr(this->adjMatr, &newMatr, this->size, newSize);
+	}
+
+	void copyMatr(int** src, int*** dest, size_t size, size_t maxSize)
+	{
+		*dest = new int*[maxSize];
+		for (size_t cnt = 0; cnt < size; ++cnt)
+		{
+			*dest[cnt] = new int[maxSize];
+		}
+		for (size_t cnt = 0; cnt < size; ++cnt)
+			for (size_t cnt2 = 0; cnt2 < size; ++cnt2)
+			{
+				if (cnt < size && cnt2 < size)
+					*dest[cnt][cnt2] = src[cnt][cnt2];
+				else
+					*dest[cnt][cnt2] = 0;
+			}
+	}
+
+public:
+	Graph(size_t maxSize = 20)
+	{
+		this->size = 0;
+		this->maxSize = maxSize;
+		this->adjMatr = new int*[this->maxSize];
+		for (size_t cnt = 0; cnt < this->maxSize; ++cnt)
+		{
+			this->adjMatr[cnt] = new int[this->maxSize];
+			for (size_t cnt2 = 0; cnt2 < this->maxSize; ++cnt2)
+			{
+				this->adjMatr[cnt][cnt2] = 0;
+			}
+		}
+	}
+	Graph(const Graph& src)
+	{
+		this->copyMatr(src.adjMatr, &this->adjMatr,src.size, src.maxSize);
+	}
+	~Graph()
+	{
+		this->deleteMatr();
+	}
+
+	void addNodes(int numOfNodes)
+	{
+		if (this->size + 1 >= this->maxSize)
+		{
+			resize(this->maxSize * 2);
+		}
+		this->size += numOfNodes;
+	}
+
+	void addEdge(size_t from, size_t to, int weigth)
+	{
+		if (from < size && to < size)
+			this->adjMatr[from][to] = weigth;
+	}
+
+	void removeEdge(size_t from, size_t to)
+	{
+		if (from < size && to < size)
+			this->adjMatr[from][to] = 0;
+	}
+
+	bool hasEdge(size_t from, size_t to)
+	{
+		if (from < size && to < size)
+			return this->adjMatr[from][to];
+		return false;
+	}
+
+	void print()
+	{
+		cout << setw(3)<< "  ";
+		for (size_t cnt = 0; cnt < this->size; ++cnt)
+		{
+			cout << setw(3) << cnt;
+		}
+		cout << endl;
+		cout << endl;
+		for (size_t cnt = 0; cnt < this->size; ++cnt)
+		{
+			cout << setw(2)<< cnt << " ";
+			for (size_t cnt2 = 0; cnt2 < this->size; ++cnt2)
+			{
+				cout << setw(3) << this->adjMatr[cnt][cnt2];
+			}
+			cout << endl;
+		}
+	}
 
 };
 
--- Dijkstra/main.cpp
+++ Dijkstra/main.cpp
@@ -1,7 +1,33 @@
-#include"heap.hpp"
+#include "heap.hpp"
+#include "graph.hpp"
+
+void inputGraph(Graph& gr)
+{
+	int from, to, n, weigth;
+	cout << "Vavedete broi varhove:" << endl;
+	cin >> n;
+	gr.addNodes(n);
+	cout << "Vavedete broi rebra:" << endl;
+	cin >> n;
+	while (n > 0)
+	{
+		cout << "Ot:" << endl;
+		cin >> from;
+		cout << "kam:" << endl;
+		cin >> to;
+		cout << "Teglo:" << endl;
+		cin >> weigth;
+
+		gr.addEdge(from, to, weigth);
+		--n;
+	}
+}
 
 int main()
 {
+	Graph gr;
+	inputGraph(gr);
+	gr.print();
 
 	return 0;
 }
